f9ebc41e4874f80f2d9c9f60d3d70d369f5e4cad
Marcio Del Raso <marciodr2m@gmail.com>
2022-07-25 22:45:47 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
-  return(
+  return(<>
     <h1>Olá Mundo!</h1>
+    <NotificationButton />
+    <NotificationButton />  
+    
+    </>
   )
 }
 
